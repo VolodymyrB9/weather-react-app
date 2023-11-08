55a893196e9a63faa885a0aef451b02f6aa2fd9a
VolodymyrB9 <volodyab97@gmail.com>
2023-11-08 22:08:17 +0200

    - reformatting get function and update app

--- src/components/WeatherContainer/WeatherForm.js
+++ src/components/WeatherContainer/WeatherForm.js
@@ -1,32 +1,44 @@
-import {useState} from "react";
-import {useForm} from "react-hook-form";
-import axios from "axios";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-import {apiKey, baseURL} from "../../constants";
+import { apiKey } from "../../constants";
+import { axiosService } from "../../services";
 import css from './WeatherForm.module.css'
+
 const WeatherForm = () => {
-    const {register,handleSubmit, formState: {errors}} = useForm();
-    const [weatherData, setWeatherData] = useState(!{
-        name: "",
-        sys: { country: "" },
-        main: { temp: "" },
-        weather: [{ description: "" }],
-    });
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [weatherData, setWeatherData] = useState(null);
+    const [error, setError] = useState(null);
+
     const onSubmit = async (data) => {
         try {
-            const res = await axios.get(baseURL+`?q=${data.city}&appid=${apiKey}&units=metric`)
-            setWeatherData(res.data)
+            const response = await getWeatherData(data.city);
+            setWeatherData(response.data);
+            setError(null);
         } catch (e) {
+            setError('Помилка при отриманні погодних даних.');
             console.error('Error fetching weather data:', e);
         }
     };
+
+    const getWeatherData = async (city) => {
+        return axiosService.get('', {
+            params: {
+                q: city,
+                appid: apiKey,
+                units: 'metric'
+            }
+        });
+    };
+
     return (
         <div className={css.Form}>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input {...register('city', {required: true})} placeholder={'Введіть назву міста'}/>
+                    <input {...register('city', { required: true })} placeholder={'Введіть назву міста'} />
                     <button type="submit">Пошук</button>
                     {errors.city && <span>Місто не визначено</span>}
+                    {error && <span>{error}</span>}
                 </form>
             </div>
             {weatherData && (
@@ -40,4 +52,4 @@ const WeatherForm = () => {
     );
 };
 
-export {WeatherForm};
+export { WeatherForm };
--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,9 +2,17 @@ const baseURL = `https://api.openweathermap.org/data/2.5/weather`;
 
 const apiKey= 'e31b08073926e29d4460d2e32cfbb83c'
 
+const urls = {
+    params: {
+     q: 'city',
+     appid: apiKey
+    }
 
+
+}
 
 export {
     baseURL,
+    urls,
     apiKey
 }
